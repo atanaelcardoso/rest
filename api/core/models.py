d796885ef501c63c647c1ca2feb38b241c35a230
Atanael Cardoso <110429151+atanaelcardoso@users.noreply.github.com>
2023-01-31 18:47:28 +0000

    ForeignKey

--- api/core/models.py
+++ api/core/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-#from django.contrib.auth import get_user_model
+from pro.models import Produto
 
 class Cliente(models.Model):
   nome = models.CharField(max_length=50)
   endereco = models.CharField(max_length=50)
   idade = models.IntegerField()
- # user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+  pro = models.ForeignKey(Produto, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
